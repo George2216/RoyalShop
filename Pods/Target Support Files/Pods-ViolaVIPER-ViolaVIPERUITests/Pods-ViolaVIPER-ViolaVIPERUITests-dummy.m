dc1f44a81c8c12753dff7dc15741e4e4db14594f
georgeChurikov <georgechurikov@gmail.com>
2022-01-05 14:44:00 +0200

    working version

---
+++ Pods/Target Support Files/Pods-ViolaVIPER-ViolaVIPERUITests/Pods-ViolaVIPER-ViolaVIPERUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ViolaVIPER_ViolaVIPERUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ViolaVIPER_ViolaVIPERUITests
+@end
---
+++ Pods/Target Support Files/Pods-ViolaVIPER-ViolaVIPERUITests/Pods-ViolaVIPER-ViolaVIPERUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_ViolaVIPER_ViolaVIPERUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ViolaVIPER_ViolaVIPERUITestsVersionString[];
+
